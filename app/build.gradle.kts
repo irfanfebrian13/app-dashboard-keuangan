611d1a95e00b5a430e6aa81def420d8081e21e9b
irfanfebrian13 <irfanifebrians13@gmail.com>
2026-06-08 15:17:49 +0700

    Bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.cantara.hematku"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "1.0.4"
+    versionCode = 5
+    versionName = "1.0.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
